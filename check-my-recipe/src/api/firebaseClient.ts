996beadeda2d8b78638263a9a70e24cb9d2beb00
Jasper Saaltink <jasper.saaltink@gmail.com>
2023-06-14 21:24:25 +0200

    the correct firebase project configuration

--- check-my-recipe/src/api/firebaseClient.ts
+++ check-my-recipe/src/api/firebaseClient.ts
@@ -6,13 +6,13 @@ import { connectFirestoreEmulator, Firestore, initializeFirestore } from 'fireba
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiXHIdbtzRQ9hmPi24NsYRU2DryqOufjQ",
-  authDomain: "characters-react.firebaseapp.com",
-  projectId: "characters-react",
-  storageBucket: "characters-react.appspot.com",
-  messagingSenderId: "1052637248245",
-  appId: "1:1052637248245:web:91fd8c54df8577b383e3bc",
-  measurementId: "G-1H87WCVSFN"
+  apiKey: "AIzaSyCNasvepjvLBJVH4ELzl37v6ag2WVtaTWo",
+  authDomain: "check-my-recipe.firebaseapp.com",
+  projectId: "check-my-recipe",
+  storageBucket: "check-my-recipe.appspot.com",
+  messagingSenderId: "408544859532",
+  appId: "1:408544859532:web:877ffd5b4120e702e132a9",
+  measurementId: "G-966J7YDS4G"
 };
 
 class FirebaseClient {

f2ccc74565e7a4b5fc59fda966c3b8b87121964e
Jasper Saaltink <jasper.saaltink@gmail.com>
2023-06-14 20:23:58 +0200

    project setup (#1)
    * project setup

---
+++ check-my-recipe/src/api/firestore-by-type/legend-character-api.ts
@@ -0,0 +1,8 @@
+// import { LegendCharacter } from '@/types/legendCharacter'
+// import { where } from 'firebase/firestore'
+// import { Api } from '../genericApi'
+
+// const COLLECTION_LEGEND_CHARACTERS = 'legend-characters'
+
+// export const legendCharacterApi = new Api<LegendCharacter>(COLLECTION_LEGEND_CHARACTERS)
+
---
+++ check-my-recipe/src/api/index.ts
@@ -0,0 +1,2 @@
+export { type ApiFor } from './genericApi'
+export * as auth from './auth'
